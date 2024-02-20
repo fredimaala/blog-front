3ddac30a151bb3209effdb0f374d50a082f2acd4
fredimaala <fredioskar.maala@tptlive.ee>
2024-02-20 12:59:44 +0200

    head isu teile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
+import { ref } from 'vue';
 
+
+let messege = ref('Hello Vue');
+
+let items = ref(['sai', 'munad', 'piim', 'viin', 'suits']);
+function add() {
+  if (messege.value.trim() !== '') {
+    items.value.push(messege.value.trim());
+  }
+  messege.value = '';
+}
 </script>
 
 <template>
-  
+  <button @click="add">Click Me</button>
+  <input type="text" v-model="messege" @keydown.enter="add">
+  <li v-for="item in items">{{ item }}</li>
 </template>
 
-<style scoped>
 
-</style>
