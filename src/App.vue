d89df47ec86edb29d90a470a1d7bf5fd76d50b0a
fredimaala <fredioskar.maala@tptlive.ee>
2024-03-05 11:52:21 +0200

    finished item list

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
-
+let i = 0;
 let messege = ref('Hello Vue');
 
-let items = ref(['sai', 'munad', 'piim', 'viin', 'suits']);
+let items = ref([
+  {id: i++, name:'sai', isDone: false},
+  {id: i++, name:'munad', isDone: false},
+  {id: i++, name:'piim', isDone: true},
+  {id: i++, name:'viin', isDone: false},
+  {id: i++, name:'suits', isDone: true},
+]);
 function add() {
   if (messege.value.trim() !== '') {
-    items.value.push(messege.value.trim());
+    items.value.push({id: i++, name:messege.value.trim(), isDone: true},);
   }
   messege.value = '';
 }
+let doneItems = computed(() => {
+  return items.value.filter(item => item.isDone);
+});
+let toDoItems = computed(() => {
+  return items.value.filter(item => !item.isDone);
+});
 </script>
-
 <template>
-  <button @click="add">Click Me</button>
-  <input type="text" v-model="messege" @keydown.enter="add">
-  <li v-for="item in items">{{ item }}</li>
+<button @click="add">Click Me</button>
+<input type="text" v-model="messege" @keydown.enter="add">
+
+<h1>All items</h1>
+<ul>
+  <li v-for="item in items" :key="item.id">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.isDone">
+  </li>
+</ul>
+
+<h1>Done items</h1>
+<ul>
+  <li v-for="item in doneItems" :key="item.id">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.isDone">
+  </li>
+</ul>
+
+<h1>Todo items</h1>
+<ul>
+  <li v-for="item in toDoItems" :key="item.id">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.isDone">
+  </li>
+</ul>
 </template>
 
 
