530f5f42b52341d66e39f1b5cfff223cbb9af4ee
fredimaala <fredioskar.maala@tptlive.ee>
2024-03-05 13:21:17 +0200

    Add buefy & MainNav

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
 import './style.css';
+import Buefy from '@ntohq/buefy-next';
+import '@ntohq/buefy-next/dist/buefy.css';
+
+
 import App from './App.vue';
 
-createApp(App).mount('#app');
+
+import Home from './pages/Home.vue'
+import ToDo from './pages/Todo.vue'
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/todo', component: ToDo },
+  ]
+
+  
+  // 3. Create the router instance and pass the `routes` option
+  // You can pass in additional options here, but let's
+  // keep it simple for now.
+  const router = createRouter({
+    // 4. Provide the history implementation to use. We
+    // are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+
+const app = createApp(App);
+app.use(router);
+app.use(Buefy)
+app.mount('#app');
+
---
+++ src/pages/Todo.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, computed } from 'vue';
+import ItemList from '../components/ItemList.vue';
+
+let i = 0;
+let messege = ref('Hello Vue');
+
+let items = ref([
+  {id: i++, name:'sai', isDone: false},
+  {id: i++, name:'munad', isDone: false},
+  {id: i++, name:'piim', isDone: true},
+  {id: i++, name:'viin', isDone: false},
+  {id: i++, name:'suits', isDone: true},
+]);
+function add() {
+  if (messege.value.trim() !== '') {
+    items.value.push({id: i++, name:messege.value.trim(), isDone: true},);
+  }
+  messege.value = '';
+}
+let doneItems = computed(() => {
+  return items.value.filter(item => item.isDone);
+});
+let toDoItems = computed(() => {
+  return items.value.filter(item => !item.isDone);
+});
+</script>
+<template>
+<button @click="add">Click Me</button>
+<input type="text" v-model="messege" @keydown.enter="add">
+
+<ItemList :items="items" title="All items"></ItemList>
+<ItemList :items="doneItems" title="Done items"></ItemList>
+<ItemList :items="toDoItems" title="Todo items"></ItemList>
+
+</template>
+
+
